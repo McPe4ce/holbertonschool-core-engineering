e815ecfb95fdaa5b456f982ceb170d4b7b4ec00d
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-04 16:11:25 +0200

    Fix print_alphabt.py to remove newline character from the alphabet string

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
-for letter in "abcdefghijklmnopqrstuvwxyz\n":
+for letter in "abcdefghijklmnopqrstuvwxyz":
     if letter != "q" and letter != "e":
         print(f"{letter}", end="")
