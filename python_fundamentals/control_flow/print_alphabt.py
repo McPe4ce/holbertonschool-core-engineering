0463e1afb2d85300ef7c8939069189b4b071b6ab
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-04 16:10:16 +0200

    Fix print_alphabt.py to include newline character in the alphabet string

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-for letter in "abcdefghijklmnopqrstuvwxyz":
+for letter in "abcdefghijklmnopqrstuvwxyz\n":
     if letter != "q" and letter != "e":
         print(f"{letter}", end="")
 
