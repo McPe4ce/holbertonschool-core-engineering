83440ee444447c65947dba504fbfcd84ef76094d
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-04 15:27:28 +0200

    Refactor last_digit.py to improve last digit calculation logic

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
 number = __import__('random').randint(-10000, 10000)
-last_n = number % 10
 
+if number >= 0:
+    last_n = number % 10
+
+else:
+    last_n = -((-number) % 10)
+    
 if last_n > 5:
     print(f"Last digit of {number} is {last_n} and is greater than 5")
 
-if last_n == 0:
+elif last_n == 0:
     print(f"Last digit of {number} is {last_n} and is 0")
 
-if last_n < 6 and last_n != 0:
+else:
     print(f"Last digit of {number} is {last_n} and is less than 6 and not 0")
