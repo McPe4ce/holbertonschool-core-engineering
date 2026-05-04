faa7a4c6bf69b39a5058daaf24f603851c039f2b
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-04 16:00:21 +0200

    Refactor print_alphabt.py to print the alphabet excluding 'q' and 'e'

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 
-print("abcdfghijklmnoprstuvwxyz")
+for letter in "abcdefghijklmnopqrstuvwxyz":
+    if letter not in "qe":
+        print(letter, end="")
+    print(end="")
+print(end="\n")
