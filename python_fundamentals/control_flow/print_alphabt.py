db5ed9a88ce9f2cd2c6734531fea298ad7d8e421
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-04 16:02:03 +0200

    Refactor print_alphabt.py to simplify letter exclusion logic and improve formatting

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
 for letter in "abcdefghijklmnopqrstuvwxyz":
-    if letter not in "qe":
-        print(letter, end="")
-    print(end="")
-print(end="\n")
+    if letter != "q" and letter != "e":
+        print("{}".format(letter), end="")
