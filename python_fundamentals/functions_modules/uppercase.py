c36d54cade5f6d4f092e89198b98bf89fe932380
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-05 14:49:51 +0200

    Refactor uppercase function to simplify loop and character handling

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 def uppercase(str):
-    for index, letter in str:
+    for letter in str:
         askey = ord(letter)
-
-        charr = (askey - 32) if 97 <= askey <= 122 else letter
-        print("{}".format(charr), end="\n" if index == len(str) - 1 else "")
+        if 97 <= askey <= 122:
+            askey -= 32
+        print("{}".format(chr(askey)), end="")
+    print()
