aff1fb390e7f4bb8438a362fcb585e928c3e06ed
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-05 14:09:42 +0200

    Add uppercase.py to convert lowercase letters to uppercase

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def uppercase(str):
+    for letter in str:
+        askey = ord(letter)
+
+        if 97 <= askey <= 122:
+            print(chr(askey - 32), end="")
+        else:
+            print(letter, end="")
+    print()
