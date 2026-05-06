0bc040b8a616c71401490a4426424cf90f2277c3
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-06 21:16:45 +0200

    Add update_dictionary function to modify dictionary entries

---
+++ python_fundamentals/core_data_structures/update_dictionary.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def update_dictionary(a_dictionary, key, value):
+    if key == None:
+        a_dictionary[key] = dict(key, value)
+    else:
+        a_dictionary[key] = value
+
+        return a_dictionary
+
+
+if __name__ == "__main__":
+    d = {'language': 'C', 'number': 89, 'track': 'Low level'}
+    print(update_dictionary(d, 'language', 'Python'))
+    print(update_dictionary(d, 'city', 'San Francisco'))
