87b3dfe8549d5d700b3fd08184a509915cf1d78c
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-11 15:10:28 +0200

    Fix formatting in safe_print_list_integers function to remove extra space in print statement

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for indexor in range(x):
             if isinstance(my_list[indexor], int):
-                print("{:d}".format(my_list[indexor], end=" "))
+                print("{:d}".format(my_list[indexor], end=""))
                 counter += 1
         print()
     except (TypeError, IndexError):
