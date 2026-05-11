264c304747811090dc1a62a2668849448fac0ee0
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-11 16:03:17 +0200

    Fix formatting in safe_print_division function to ensure consistent output

--- python_fundamentals/exception_handling/safe_print_division.py
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -7,7 +7,7 @@ def safe_print_division(a, b):
         result = None
         return result
     finally:
-        print("Inside result {}".format(result))
+        print("Inside result {}".format(result), end=" ")
     return result
 
 
