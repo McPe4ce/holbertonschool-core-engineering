c69cbc2ad5a2012902d500533c38a94032cfeca1
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-11 15:09:41 +0200

    Fix formatting in safe_print_list_integers function and update comment style

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-#prints the first x elements of a list
+# prints the first x elements of a list
 
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
     try:
         for indexor in range(x):
             if isinstance(my_list[indexor], int):
-                print(my_list[indexor], end=" ")
+                print("{:d}".format(my_list[indexor], end=" "))
                 counter += 1
         print()
     except (TypeError, IndexError):
