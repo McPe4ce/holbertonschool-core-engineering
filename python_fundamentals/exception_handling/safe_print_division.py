2fae12e847d8d0d9025727ea263ce9cc3191a394
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-11 16:04:33 +0200

    Fix formatting in safe_print_division function to improve output clarity

--- python_fundamentals/exception_handling/safe_print_division.py
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -7,7 +7,7 @@ def safe_print_division(a, b):
         result = None
         return result
     finally:
-        print("Inside result {}".format(result), end=" ")
+        print("Inside result: {}".format(result))
     return result
 
 
