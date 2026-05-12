61c4ac342707a84c832518ab8e5f7765a59ae6be
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-12 15:46:24 +0200

    Add Square class implementation with private size attribute and area method

---
+++ python_oop/classes_and_object_model/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Module that creates the Square class putting the size as private"""
+
+
+class Square:
+    """Represents the square and its size"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+
+if __name__ == "__main__":
+    my_square = Square(89)
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+    my_square.size = 3
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+    try:
+        my_square.size = "5 feet"
+        print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+    except Exception as e:
+        print(e)
