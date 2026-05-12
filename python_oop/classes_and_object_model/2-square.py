03638ac5a821179c1965c5e3655e69ccb5a712fd
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-12 13:19:47 +0200

    Fix formatting issues in Square class constructor and main block

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -7,13 +7,13 @@ class Square:
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        
+
         if size < 0:
             raise ValueError("size must be >= 0")
-        
+
         self.__size = size
 
-        
+
 if __name__ == "__main__":
     my_square_1 = Square(3)
     print(type(my_square_1))
