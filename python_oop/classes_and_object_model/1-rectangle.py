a2504908190d01326d02a62f4bfdb926a2d3d9b8
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-13 16:58:40 +0200

    Add module and class docstrings for Rectangle class

--- python_oop/classes_and_object_model/1-rectangle.py
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+"""Module that defines a Rectangle class."""
 
 class Rectangle:
+    """Represents a Rectangle and its height/width"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
