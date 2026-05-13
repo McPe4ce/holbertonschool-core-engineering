323a0ad11a05d5f68346ce02c42a058ef9d1b422
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-13 15:40:39 +0200

    Add Square class with private size and position attributes, area calculation, and printing method

---
+++ python_oop/classes_and_object_model/6-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""Module that creates the Square class putting the size as private"""
+
+
+class Square:
+    """Represents the square and its size"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+
+        # Print col
+        for _ in range(self.__position[1]):
+            print()
+
+        # Print row
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integer")
+        for index in value:
+            if not all(isinstance(index, int) or index >= 0):
+                raise TypeError("position must be tuple of 2 positive integer")
+        self.__position = value
+
+    def __str__(self):
+        result = ""
+
+        # Add vertical offset (empty lines)
+        for _ in range(self.__position[1]):
+            result += "\n"
+
+        # Add the square with horizontal offset
+        for i in range(self.__size):
+            result += " " * self.__position[0] + "#" * self.__size
+            if i < self.__size - 1:
+                result += "\n"
+
+        return result
