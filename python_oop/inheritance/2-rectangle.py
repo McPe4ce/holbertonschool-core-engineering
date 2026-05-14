6da33d566f2d9ce374ad7357786c0a311017bece
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-14 13:08:56 +0200

    Add Rectangle class inheriting from BaseGeometry with area and string representation methods

---
+++ python_oop/inheritance/2-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inheriting from BaseGeometry"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
