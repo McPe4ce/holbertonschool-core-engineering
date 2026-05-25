c530ae5a84eb4a67e53ffca94c3d7d1dfcb472ac
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-25 11:39:28 +0200

    Add docstring to append_write function to clarify file opening mode

--- python_advanced/file_handling/append_write.py
+++ python_advanced/file_handling/append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-
+"""Module that appends a text to a file"""
 
 def append_write(filename="", text=""):
+    """'a' opens the file in append mode"""
     with open(f"{filename}", 'a') as f:
         return f.write(text)
