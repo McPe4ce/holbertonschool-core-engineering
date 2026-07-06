1903444b5b75e8ab6a7061a9eb7e472bc692faf9
Mcpe4ce <phil.ghanem@gmail.com>
2026-07-06 16:35:45 +0200

    refactor: update connect_and_send function to accept parameters for URI and message

--- python_advanced/websockets/ws_client.py
+++ python_advanced/websockets/ws_client.py
@@ -4,12 +4,12 @@ import asyncio
 from websockets.asyncio.client import connect
 
 
-async def connect_and_send():
-    async with connect("ws://localhost:8765") as websocket:
-        await websocket.send("Hello WebSocket")
-        message = await websocket.recv()
-        print(message)
+async def connect_and_send(uri, message):
+    async with connect(uri) as websocket:
+        await websocket.send(message)
+        response = await websocket.recv()
+        print(response)
 
 
 if __name__ == "__main__":
-    asyncio.run(connect_and_send())
+    asyncio.run(connect_and_send("ws://localhost:8765", "Hello WebSocket"))
