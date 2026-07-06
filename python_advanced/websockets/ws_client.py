903d54f4026770b32c812d0bad8701e9bd4880cf
Mcpe4ce <phil.ghanem@gmail.com>
2026-07-06 16:37:08 +0200

    refactor: modify connect_and_send to return response instead of printing

--- python_advanced/websockets/ws_client.py
+++ python_advanced/websockets/ws_client.py
@@ -8,8 +8,8 @@ async def connect_and_send(uri, message):
     async with connect(uri) as websocket:
         await websocket.send(message)
         response = await websocket.recv()
-        print(response)
+        return response
 
 
 if __name__ == "__main__":
-    asyncio.run(connect_and_send("ws://localhost:8765", "Hello WebSocket"))
+    print(asyncio.run(connect_and_send("ws://localhost:8765", "Hello WebSocket")))
