a4fed54d5d8704f5a1b7962da4d4a8b5b313f6f3
Mcpe4ce <phil.ghanem@gmail.com>
2026-07-06 16:33:34 +0200

    feat: add WebSocket client implementation

---
+++ python_advanced/websockets/ws_client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import asyncio
+from websockets.asyncio.client import connect
+
+
+async def hello():
+    async with connect("ws://localhost:8765") as websocket:
+        await websocket.send("Hello WebSocket")
+        message = await websocket.recv()
+        print(message)
+
+
+if __name__ == "__main__":
+    asyncio.run(hello())
