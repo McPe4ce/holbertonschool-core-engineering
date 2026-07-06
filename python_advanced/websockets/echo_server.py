5f30f2b5352f5df67ad802f5135ffd4d60aa7336
Mcpe4ce <phil.ghanem@gmail.com>
2026-07-06 15:25:40 +0200

    Add echo server implementation using websockets

---
+++ python_advanced/websockets/echo_server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from websockets.asyncio.server import serve
+import asyncio
+
+
+async def echo(websocket):
+    async for message in websocket:
+        await websocket.send(message)
+
+
+async def main():
+    async with serve(echo, "localhost", 8765) as server:
+        await server.serve_forever()
+
+if __name__ == "__main__":
+    asyncio.run(main())
