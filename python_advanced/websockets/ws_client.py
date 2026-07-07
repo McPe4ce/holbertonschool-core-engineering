9426ff31d2c749a69cf1290f262a357a291fae83
Mcpe4ce <phil.ghanem@gmail.com>
2026-07-07 09:49:28 +0200

    refactor: update main execution to use environment variable for URI and command line argument for message

--- python_advanced/websockets/ws_client.py
+++ python_advanced/websockets/ws_client.py
@@ -2,6 +2,8 @@
 
 import asyncio
 from websockets.asyncio.client import connect
+import os
+import sys
 
 
 async def connect_and_send(uri, message):
@@ -12,4 +14,7 @@ async def connect_and_send(uri, message):
 
 
 if __name__ == "__main__":
-    print(asyncio.run(connect_and_send("ws://localhost:8765", "Hello WebSocket")))
+    uri = os.environ.get("WS_URI", "ws://localhost:8765")
+    message = sys.argv[1]
+    websocks = asyncio.run(connect_and_send(uri, message))
+    sys.stdout.write(websocks)
