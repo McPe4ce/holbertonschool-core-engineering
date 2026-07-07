5cca747e7400e7e038fd27b853554160de695673
Mcpe4ce <phil.ghanem@gmail.com>
2026-07-07 16:50:26 +0200

    feat: implement WebSocket server and client HTML for real-time communication

---
+++ python_advanced/websockets/asgi_server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from starlette.applications import Starlette
+from starlette.responses import FileResponse
+from starlette.routing import WebSocketRoute, Route
+from starlette.websockets import WebSocketDisconnect
+
+
+
+async def homepage(request):
+    return FileResponse("index.html")
+
+
+async def websocket_endpoint(websocket):
+    await websocket.accept()
+    try:
+        while True:
+            message = await websocket.receive_text()
+            if not message:
+                print("ERR: EMPTY")
+                continue
+            await websocket.send_text(message)
+    except WebSocketDisconnect:
+        pass
+        
+
+
+app = Starlette(debug=True, routes= [
+    WebSocketRoute('/ws', websocket_endpoint),
+    Route('/', homepage),
+])
